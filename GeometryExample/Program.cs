cb3933301ad39dfc11874275535388f46628f013
MykytaChystyi <neyton61@gmail.com>
2024-11-17 13:53:53 +0200

    Додав 2 песочних приклада використання наслідування

---
+++ GeometryExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public abstract class GeometryFigure
+    {
+        public int PointX;
+        public virtual int Area() => 0; 
+    }
+    public class Square : GeometryFigure
+    {
+        public int PointY;
+
+        public override int Area()
+        {
+            var piece = GetPiece();
+            return piece * piece;
+        }
+        public int GetPiece() => Math.Abs(PointX - PointY);
+    }
+    public class Triangle : Square
+    {
+        public int PointZ;
+
+        // S = √p(p - a)(p - b)(p - c) =
+        public virtual new double Area()
+        {
+            var p = Perimeter();
+            return Math.Sqrt(p * (p - PointX) * (p - PointY) * (p - PointZ));
+        }
+        // p = (a + b + c) / 2 
+        public double Perimeter() => (PointX + PointY + PointZ) / 2;
+    }
+    public class Ellipse : Square
+    {
+        // S = π a b = π · 2 · 3 = 6 π (м)2 ≈ 18.84 (м)2
+        public new double Area() => Math.PI * PointX * PointY;
+    }
+    public class Rectange : Triangle
+    {
+        public new int Area() => GetFirstPiece() * GetSecondPiece();
+        public int GetFirstPiece() => Math.Abs(PointX - PointY);
+        public int GetSecondPiece() => Math.Abs(PointX - PointZ);
+    }
+}
---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TripleACalls();
+            UniqueCalls();
+            InheritanceCalls();
+            Console.Read();
+        }
+        static void TripleACalls()
+        {
+            A aa = new A();
+            A ab = new B();
+            A ac = new C();
+            aa.Method();
+            ab.Method();
+            ac.Method();
+            Console.WriteLine("------------------------------");
+        }
+        static void UniqueCalls()
+        {
+            A aa = new A();
+            B bb = new B();
+            C cc = new C();
+            aa.Method();
+            bb.Method();
+            cc.Method();
+            Console.WriteLine("------------------------------");
+        }
+        static void InheritanceCalls()
+        {
+            A a = new B();
+            B b = new B();
+            B b2 = new C();
+            A c = new C();
+            a.Method();
+            b.Method();
+            b2.Method(); // because of override option
+            c.Method();
+            Console.WriteLine("------------------------------");
+        }
+    }
+    public class A
+    {
+        public virtual void Method()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+    public class B : A
+    {
+        public virtual new void Method()
+        {
+            Console.WriteLine("Class B");
+        }
+    }
+    public class C : B
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Class C");
+        }
+    }
+}
